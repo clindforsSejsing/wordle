9c989101b38581d813042761aa672c9fa4980226
CLS <clindforsSejsing@gmail.com>
2022-03-09 17:40:54 +0100

    added a file with words that have 5 letters in it, in swedish

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { wordsArray } from "./word.js";
 //* Make a game of wordle:
 /* 1. The game chose a word from a list and make the user know how many letters the word has. 
 2. The user guesses by writing into an input, a word with the same amount of letters.
@@ -6,3 +7,7 @@
 5. The input with the correct letters (and at the right place) should stay in place when the user gets more chanses to guess the word. 
 6. The guesses have no limits. The user gets a notification when the correct word is guessed. 
 *///*@author CLS
+
+console.log(wordsArray);
+console.log(wordsArray.length);
+console.log(wordsArray[10]);
